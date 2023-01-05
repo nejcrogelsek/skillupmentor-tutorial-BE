020daee0a3294df4707105e2c9a0420bb7b95fcb
nejcrogelsek <nejcrogelsek0@gmail.com>
2023-01-05 18:24:56 +0100

    remove unused items

--- src/types/types.d.ts
+++
@@ -1,7 +0,0 @@
-export interface DefaultJWT extends Record<string, unknown> {
-  name?: string | null
-  email?: string | null
-  sub?: string
-}
-
-export interface JWT extends Record<string, unknown>, DefaultJWT {}
